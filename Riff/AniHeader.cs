6c9e153670493cb926bc8c9b82960b4f2eb1ed32
gisellevonbingen <gisellevonbingen@gmail.com>
2022-06-24 17:10:15 +0900

    Add AniHeader, KnownTypeKeys

---
+++ Riff/AniHeader.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Riff
+{
+    public class IcoAniHeader
+    {
+        public int Frames { get; set; }
+        public int Steps { get; set; }
+        public int CenterX { get; set; }
+        public int CenterY { get; set; }
+        public int BitCount { get; set; }
+        public int Planes { get; set; }
+        public int JIFRate { get; set; }
+        public int Flags { get; set; }
+
+        public IcoAniHeader()
+        {
+
+        }
+
+        public IcoAniHeader(Stream input) : this()
+        {
+            this.Read(input);
+        }
+
+        public void Read(Stream input)
+        {
+            var processor = RiffChunk.CreateRiffDataProcessor(input);
+            var sizeWithSelf = processor.ReadInt();
+            this.Frames = processor.ReadInt();
+            this.Steps = processor.ReadInt();
+            this.CenterX = processor.ReadInt();
+            this.CenterY = processor.ReadInt();
+            this.BitCount = processor.ReadInt();
+            this.Planes = processor.ReadInt();
+            this.JIFRate = processor.ReadInt();
+            this.Flags = processor.ReadInt();
+        }
+
+        public void Write(Stream output)
+        {
+            var processor = RiffChunk.CreateRiffDataProcessor(output);
+            processor.WriteInt(0x24);
+            processor.WriteInt(this.Frames);
+            processor.WriteInt(this.Steps);
+            processor.WriteInt(this.CenterX);
+            processor.WriteInt(this.CenterY);
+            processor.WriteInt(this.BitCount);
+            processor.WriteInt(this.Planes);
+            processor.WriteInt(this.JIFRate);
+            processor.WriteInt(this.Flags);
+        }
+
+    }
+
+}
--- Riff/KnownRiffTypeKeys.cs
+++ Riff/KnownRiffTypeKeys.cs
@@ -9,6 +9,22 @@ namespace Riff
         public const int Riff = 0x46464952;
         public const int List = 0x5453494C;
 
+        public const int Info = 0x4F464E49;
+        public const int InfoName = 0x4D414E49;
+        public const int InfoCopyright = 0x504F4349;
+        public const int InfoArtist = 0x54524149;
+
+        /// <summary>
+        /// Windows NT Animated Cursor RIFF Files
+        /// </summary>
+        public const int Acon = 0x4E4F4341;
+
+        public const int AniHeader = 0x68696E61;
+        public const int Rate = 0x65746172;
+        public const int Sequence = 0x20716573;
+        public const int Frame = 0x6D617266;
+        public const int Icon = 0x6E6F6369;
+
     }
 
 }
